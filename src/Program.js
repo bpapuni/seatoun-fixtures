277ad7947a97113cb92c7e392ebc33199bea0b13
Brent Papuni <brent.papuni@gmail.com>
2024-04-28 15:29:48 +1200

    Displays programs for next 4 weeks

--- src/Program.js
+++ src/Program.js
@@ -3,17 +3,7 @@ import './styles/fonts.css';
 import './styles/program.css';
 
 function Program() {
-    const suffixes = ["th", "st", "nd", "rd"];
-    const comps = [
-        "Women's Central League",
-        "Men's Capital Premier",
-        "Men's Capital 2",
-        "Women's Capital 1",
-        "Women's Capital 3",
-        "Masters 1",
-        "Masters Over 45's - 1",
-        "Masters Over 45's - 2",
-    ];
+    const numOfWeeks = 4;
 
     const today = new Date();
     const currentDay = today.getDay();
@@ -29,13 +19,27 @@ function Program() {
     nextSunday.setHours(23, 59, 59, 999);
     nextSunday.setMinutes(nextSunday.getMinutes() - nextSunday.getTimezoneOffset());
 
+    const endDay = new Date(nextSunday);
+    endDay.setDate(endDay.getDate() + numOfWeeks * 7);
+
+    const comps = [
+        "Women's Central League",
+        "Men's Capital Premier",
+        "Men's Capital 2",
+        "Women's Capital 1",
+        "Women's Capital 3",
+        "Masters 1",
+        "Masters Over 45's - 1",
+        "Masters Over 45's - 2",
+    ];
+
     const [dataCache, setDataCache] = useState([]);
 
     const fetchData = async (comp) => {
         try {
           const params = {
             from: nextMonday.toISOString(),
-            to: nextSunday.toISOString()
+            to: endDay.toISOString()
           };
           
           const url = "/api/v1/fixtures?";
@@ -59,28 +63,56 @@ function Program() {
         }
     };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+    const FormatDate = (date) => {
+        const newDate = new Date(date);
+        const day = newDate.toLocaleString("en-NZ", {
+            day: "numeric",
+        });
+        const month = newDate.toLocaleString("en-NZ", {
+            month: "short",
+        });
 
-    return (
-        <div id="program">
-            {comps.map((compName, i) => (
-                <div key={i}>
-                    <hr />
-                    <h1>{compName}</h1>
-                    {dataCache.filter(fixture => fixture.comp == compName).map((fixture, j) => (
+        const suffixes = ["th", "st", "nd", "rd"];
+        
+        let suffix;
+        if (day >= 11 && day <= 13) {
+            suffix = "th"; // Use "th" for 11th, 12th, and 13th
+        } else {
+            suffix = suffixes[day % 10] || "th"; // Use the appropriate suffix or default to "th"
+        }
+
+        return day + suffix + " " + month;
+    }
+
+    const RenderWeeks = (index) => {
+        const weeks = [];
+
+        for(let i = 0; i < index; i++) {
+            const nextMonday = new Date(today);
+            const daysUntilMonday = currentDay === 0 ? 1 : 8 - currentDay;
+            nextMonday.setDate(today.getDate() + daysUntilMonday + i * 7);
+            nextMonday.setHours(0, 0, 0, 0);
+            nextMonday.setMinutes(nextMonday.getMinutes() - nextMonday.getTimezoneOffset());
+    
+            const nextSunday = new Date(nextMonday + i * 7);
+            nextSunday.setDate(nextMonday.getDate() + 6);
+            nextSunday.setHours(23, 59, 59, 999);
+            nextSunday.setMinutes(nextSunday.getMinutes() - nextSunday.getTimezoneOffset());
+
+            weeks.push(
+                <div className="program" key={i}>
+                    <h2>{FormatDate(nextMonday)} - {FormatDate(nextSunday)}</h2>
+                    {dataCache.filter(fixture => ((new Date(fixture.fixtureDate) >= nextMonday) && (new Date(fixture.fixtureDate) <= nextSunday))).sort((a, b) => new Date(a.fixtureDate) - new Date(b.fixtureDate)).map((fixture, j) => (
                         <div key={j}>
+                            <hr />
+                            <h1>{fixture.comp}</h1>
                             <p>{fixture.homeTeam.includes("Seatoun") ? fixture.awayTeam : fixture.homeTeam}</p>
                             <p> 
-                                {new Date(fixture.fixtureDate).toLocaleString('en-NZ', {
-                                    day: 'numeric',
-                                    month: 'short',
-                                })}
+                                {FormatDate(fixture.fixtureDate)}
                                 {" "}
-                                {new Date(fixture.fixtureDate).toLocaleString('en-NZ', {
-                                    hour: 'numeric',
-                                    minute: '2-digit',
+                                {new Date(fixture.fixtureDate).toLocaleString("en-NZ", {
+                                    hour: "numeric",
+                                    minute: "2-digit",
                                     hour12: true,
                                 })}
                             </p>
@@ -88,7 +120,19 @@ function Program() {
                         </div>
                     ))}
                 </div>
-            ))}
+            )
+        }
+
+        return weeks;
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div id="programs">
+            {RenderWeeks(numOfWeeks)}
         </div>
     )
 }
