32a3673d2ad196bfe4565b1c622413b72aa3763c
bpapuni <brent.papuni@gmail.com>
2024-07-05 10:27:44 +1200

    Remove Bye games from displaying

--- src/components/Fixture.jsx
+++ src/components/Fixture.jsx
@@ -1,39 +1,41 @@
 const Fixture = ({ match, i, nextMatch}) => {
     return (
         <>
-            <div key={i} className={nextMatch ? "fixture font-normal next-match" : "fixture font-normal" }>
-                <span>
-                    <div className="teams">
-                        <span>
-                            <img className="logo" src={"https:" + match.homeLogo} alt={match.homeTeam + "'s Logo"} />
-                            <p>
-                                {match.homeTeam}
-                                <strong>{match.homeScore}</strong>
-                            </p>
-                        </span>
-                        <span>
-                            <img className="logo" src={"https:" + match.awayLogo} alt={match.awayTeam + "'s Logo"} />
-                            <p>
-                                {match.awayTeam}
-                                <strong>{match.awayScore}</strong>
-                            </p>
-                        </span>
-                    </div>
-                    <div className="date">
-                        <p>{new Date(match.fixtureDate).toLocaleString('en-NZ', {
-                            day: 'numeric',
-                            month: 'short',
-                        })}</p>
-                        <p>{new Date(match.fixtureDate).toLocaleString('en-NZ', {
-                            hour: 'numeric',
-                            minute: '2-digit',
-                            hour12: true,
-                        })}</p>
-                    </div>
-                </span>
-                <strong>{match.penaltyScore && ("Penalties " + match.penaltyScore)}</strong>
-                <a href={match.map} target="_blank" rel="noreferrer" >{match.venue}</a>
-            </div>
+            {match.homeTeam != "Bye" && match.awayTeam != "Bye" && 
+                <div key={i} className={nextMatch ? "fixture font-normal next-match" : "fixture font-normal" }>
+                    <span>
+                        <div className="teams">
+                            <span>
+                                <img className="logo" src={"https:" + match.homeLogo} alt={match.homeTeam + "'s Logo"} />
+                                <p>
+                                    {match.homeTeam}
+                                    <strong>{match.homeScore}</strong>
+                                </p>
+                            </span>
+                            <span>
+                                <img className="logo" src={"https:" + match.awayLogo} alt={match.awayTeam + "'s Logo"} />
+                                <p>
+                                    {match.awayTeam}
+                                    <strong>{match.awayScore}</strong>
+                                </p>
+                            </span>
+                        </div>
+                        <div className="date">
+                            <p>{new Date(match.fixtureDate).toLocaleString('en-NZ', {
+                                day: 'numeric',
+                                month: 'short',
+                            })}</p>
+                            <p>{new Date(match.fixtureDate).toLocaleString('en-NZ', {
+                                hour: 'numeric',
+                                minute: '2-digit',
+                                hour12: true,
+                            })}</p>
+                        </div>
+                    </span>
+                    <strong>{match.penaltyScore && ("Penalties " + match.penaltyScore)}</strong>
+                    <a href={match.map} target="_blank" rel="noreferrer" >{match.venue}</a>
+                </div>
+            }
         </>
     );
 };
